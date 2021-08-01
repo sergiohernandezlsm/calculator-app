d5028b15b91aa84e2e678db115c3a06230a70378
Sergio <sergio.hernandez@littlestarmedia.com>
2021-08-01 17:12:14 +0100

    last type

--- src/helpers/dispatcher.tsx
+++ src/helpers/dispatcher.tsx
@@ -1,6 +1,7 @@
+import { Dispatch } from 'redux';
 import { ProductTypes, FieldTypes } from '../types';
 
-export const dispatchValues = (e: React.ChangeEvent<HTMLInputElement> | null, reference: string, dispatch: any, data: (ProductTypes | FieldTypes)[] | number | null) => {
+export const dispatchValues = (e: React.ChangeEvent<HTMLInputElement> | null, reference: string, dispatch: Dispatch, data: (ProductTypes | FieldTypes)[] | number | null) => {
   if (e) {
     if (e.target.name === reference) {
       dispatch({ type: reference, payload: +e.target.value });
